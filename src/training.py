d2429b2c2521be75fbf3d7db48c62dad256fde75
Monika Bhargav <monikabhargav11@gmail.com>
2021-10-17 23:49:43 +0530

    Training of ANN in python modular style

--- src/training.py
+++ src/training.py
@@ -1,11 +1,27 @@
 from yaml import parse
 from src.utils.common import read_config
+from src.utils.data_mgmt import get_data
+from src.utils.model import create_model  
 import argparse
 
 
 def training(config_path):
     config = read_config(config_path)
-    print(config)
+    validation_datasize = config["params"]["validation_datasize"]
+    (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
+
+    LOSS_FUNCTION = config["params"]["loss_functions"]
+    OPTIMIZER = config["params"]["optimizer"]
+    METRICS = config["params"]["metrics"]
+    NUM_CLASSES = config["params"]["num_classes"]
+
+    model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES)
+
+    EPOCHS = config["params"]["epochs"]
+    VALIDATION_SET = (X_valid, y_valid)
+
+    history = model.fit(X_train, y_train, epochs=EPOCHS,
+                    validation_data=VALIDATION_SET)
 
 
 
